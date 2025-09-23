edf45c4699e6c94f10dfe5dd27b44b8dc2a626ac
Aiden Bello <acanine4100@gmail.com>
2025-09-23 10:20:07 -0400

    Add MediPortal project files

---
+++ src/Library.MediPortal/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.MediPortal;
+
+public class Patient
+{
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? BirthDate { get; set; }
+    public string? Race { get; set; }
+    public string? Gender { get; set; }
+    public int Id { get; set; }
+
+    public List<string?> Diagnoses { get; set; } = new List<string?>();
+
+}
---
+++ src/Library.MediPortal/Models/Physician.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Library.MediPortal.Models;
+
+public class Physician
+{
+    public string? Name { get; set; }
+    public string? LicenseNumber { get; set; }
+    public string? GraduationDate { get; set; }
+    public string? Specializations { get; set; }
+    
+}
