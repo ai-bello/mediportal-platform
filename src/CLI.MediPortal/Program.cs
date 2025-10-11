448ed179f8a4473e181c7aa4900b588e4fc42d59
Aiden Bello <acanine4100@gmail.com>
2025-10-11 17:58:02 -0400

    Update Program.cs

--- src/CLI.MediPortal/Program.cs
+++ src/CLI.MediPortal/Program.cs
@@ -12,6 +12,23 @@ class Program
         List<Patient?> patientList = new List<Patient?>();
         List<Physician?> physicianList = new List<Physician?>();
         MainMenu();
+        bool quit = false;
+        do
+        {
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    //ManagePatients();
+                    break;
+                case "2":
+                    //ManagePhysicians();
+                    break;
+                case "3":
+                    quit = true;
+                    break;
+            }
+        } while (!quit);
+        
     }
 
     public static void MainMenu()
