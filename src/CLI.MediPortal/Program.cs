f87aa9f3afb6e97e41a7d842bf92d168d50fe851
Aiden Bello <acanine4100@gmail.com>
2025-10-11 21:15:20 -0400

    Added patient management loop

--- src/CLI.MediPortal/Program.cs
+++ src/CLI.MediPortal/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Net.Quic;
+using System.Runtime.InteropServices;
 using Library.MediPortal;
 using Library.MediPortal.Models;
 
@@ -11,14 +12,14 @@ class Program
         Console.WriteLine("\nWelcome to MediPortal!");
         List<Patient?> patientList = new List<Patient?>();
         List<Physician?> physicianList = new List<Physician?>();
-        MainMenu();
         bool quit = false;
         do
         {
+            MainMenu();
             switch (Console.ReadLine())
             {
                 case "1":
-                    //ManagePatients();
+                    ManagePatients();
                     break;
                 case "2":
                     //ManagePhysicians();
@@ -31,7 +32,6 @@ class Program
                     break;
             }
         } while (!quit);
-        
     }
 
     public static void MainMenu()
@@ -41,4 +41,51 @@ class Program
         Console.WriteLine("2. Manage Physicians");
         Console.WriteLine("3. Quit");
     }
+
+    public static void PrintPatientMenu()
+    {
+        Console.WriteLine("\n-------Patients Menu-------");
+        Console.WriteLine("1. Create new patient");
+        Console.WriteLine("2. Print list of patients");
+        Console.WriteLine("3. Update existing patient");
+        Console.WriteLine("4. Delete existing patient");
+        Console.WriteLine("5. Back to main menu");
+    }
+
+    public static void ManagePatients()
+    {
+        bool goBack = false;
+        do
+        {
+            PrintPatientMenu();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    goBack = true;
+                    break;
+            }
+        } while (!goBack);
+    }
+
+    public static void CreatePatient()
+    {
+        Patient patient = new Patient();
+        Console.WriteLine("Enter the new patient's name: ");
+        patient.Name = Console.ReadLine();
+        Console.WriteLine("Enter the patients address: ");
+        patient.Address = Console.ReadLine();
+        int birthYear = int.Parse(Console.ReadLine() ?? "-1");
+        int birthMonth = int.Parse(Console.ReadLine() ?? "-1");
+        int birthDay = int.Parse(Console.ReadLine() ?? "-1");
+        
+    }
+    
 }
