715548f1399055507f8ad2f1e971e231f7cb17d9
Aiden <105905456+ai-bello@users.noreply.github.com>
2025-10-13 21:07:31 -0400

    Implement "Create new patient" menu option (#2)
    * Added default case to the main program switch statement to handle invalid menu inputs
    
    * Added patient management loop
    
    * Added create functionality for the patient management menu in the CLI. 
    
    Closes #1

--- src/CLI.MediPortal/Program.cs
+++ src/CLI.MediPortal/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Concurrent;
+using System.Net.Quic;
+using System.Runtime.InteropServices;
 using Library.MediPortal;
 using Library.MediPortal.Models;
 
@@ -11,7 +13,26 @@ class Program
         Console.WriteLine("\nWelcome to MediPortal!");
         List<Patient?> patientList = new List<Patient?>();
         List<Physician?> physicianList = new List<Physician?>();
-        MainMenu();
+        bool quit = false;
+        do
+        {
+            MainMenu();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ManagePatients(patientList);
+                    break;
+                case "2":
+                    //ManagePhysicians();
+                    break;
+                case "3":
+                    quit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry. Try again.");
+                    break;
+            }
+        } while (!quit);
     }
 
     public static void MainMenu()
@@ -21,4 +42,90 @@ class Program
         Console.WriteLine("2. Manage Physicians");
         Console.WriteLine("3. Quit");
     }
+
+    public static void PrintPatientMenu()
+    {
+        Console.WriteLine("\n-------Patients Menu-------");
+        Console.WriteLine("1. Create new patient");
+        Console.WriteLine("2. Print list of patients");
+        Console.WriteLine("3. Update existing patient");
+        Console.WriteLine("4. Delete existing patient");
+        Console.WriteLine("5. Back to main menu");
+    }
+
+    public static void ManagePatients(List<Patient?> patientList)
+    {
+        bool goBack = false;
+        do
+        {
+            PrintPatientMenu();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    CreatePatient(patientList);
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    goBack = true;
+                    break;
+            }
+        } while (!goBack);
+    }
+
+    public static void CreatePatient(List<Patient?> patientList)
+    {
+        Patient patient = new Patient();
+        Console.WriteLine("Enter the new patient's name: ");
+        patient.Name = Console.ReadLine();
+        Console.WriteLine("Enter the patients address: ");
+        patient.Address = Console.ReadLine();
+        Console.WriteLine("Enter the patient's birthday (MM-DD-YYYY): ");
+        DateTime bd; //variable that TryParse will accept
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (DateTime.TryParse(input, out bd))
+            {
+                patient.BirthDate = bd;
+                Console.WriteLine("Successful entry.");
+                break;
+            }
+            Console.WriteLine("Invalid entry. Try again.");
+        }
+        Console.WriteLine("Enter the patient's race: ");
+        patient.Race = Console.ReadLine();
+        Console.WriteLine("Enter the patient's gender: ");
+        patient.Gender = Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("Enter the patient's diagnoses and enter 'x' if done: ");
+            string? input = Console.ReadLine();
+            if (input?.ToLower() != "x")
+            {
+                patient.Diagnoses.Add(input);
+            }
+            else
+            {
+                break;
+            }
+        }
+        var maxId = -1;
+        if (patientList.Any())
+        {
+            patientList.Select(p => p?.Id ?? 0).Max();
+        }
+        else
+        {
+            maxId = 0;
+        }
+        patient.Id = ++maxId;
+
+        patientList.Add(patient);
+    }
+    
 }
