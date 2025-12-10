10578f9f447fca1963e706701a949f331c9f92d1
Aiden <105905456+ai-bello@users.noreply.github.com>
2025-12-10 01:13:00 -0500

    Created Maui project (#29)

---
+++ src/Maui.MediPortal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui.MediPortal;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
