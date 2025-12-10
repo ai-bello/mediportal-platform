80d3336449423f729b09b03ab56ee9fd775e3dc7
Aiden <105905456+ai-bello@users.noreply.github.com>
2025-12-09 19:42:27 -0500

    Created singleton for PatientServiceProxy (#27)
    PatientServiceProxy's singleton is thread safe

---
+++ src/Library.MediPortal/Services/PatientServiceProxy.cs
@@ -0,0 +1,65 @@
+using System;
+using System.ComponentModel;
+
+namespace Library.MediPortal.Services;
+
+public class PatientServiceProxy
+{
+    private List<Patient?> patientList;
+
+    private PatientServiceProxy()
+    {
+        patientList = new List<Patient?>();
+    }
+    private static PatientServiceProxy? instance;
+    public static PatientServiceProxy Current
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new PatientServiceProxy();
+            }
+            return instance;
+        }
+    }
+
+    public List<Patient?> Patients
+    {
+        get
+        {
+            return patientList;
+        }
+
+    }
+
+    public Patient? Create(Patient? patient)
+    {
+        if (patient == null)
+        {
+            return null;
+        }
+        
+        var maxId = -1;
+        if (patientList.Any())
+        {
+            maxId = patientList.Select(p => p?.Id ?? -1).Max();
+        }
+        else
+        {
+            maxId = 0;
+        }
+        patient.Id = ++maxId;
+
+        patientList.Add(patient);
+
+        return patient;
+    }
+
+    public Patient? Delete(int id)
+    {
+        Patient? patientToDelete = patientList.Where(p => p != null).FirstOrDefault(p => p?.Id == id);
+        patientList.Remove(patientToDelete);
+        return patientToDelete;
+    }
+}
