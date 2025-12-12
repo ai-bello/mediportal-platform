4f97ba89a927254a6856330689cbf77b93a34f30
Aiden Bello <acanine4100@gmail.com>
2025-12-12 03:09:38 -0500

    Created add functionality for Patients

---
+++ src/Maui.MediPortal/ViewModels/PatientViewModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using Library.MediPortal;
+using Library.MediPortal.Services;
+
+namespace Maui.MediPortal.ViewModels;
+
+public class PatientViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<Patient?> Patients
+    {
+        get
+        {
+            return new ObservableCollection<Patient?>(PatientServiceProxy.Current.Patients);
+        }
+    }
+
+    public void Refresh()
+    {
+        NotifyPropertyChanged("Patients");
+    }
+
+    public Patient? SelectedPatient
+    {
+        get; set;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- src/Maui.MediPortal/Views/PatientsView.xaml.cs
+++ src/Maui.MediPortal/Views/PatientsView.xaml.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using Maui.MediPortal.ViewModels;
+
 namespace Maui.MediPortal.Views;
 
 public partial class PatientsView : ContentPage
@@ -5,14 +8,20 @@ public partial class PatientsView : ContentPage
 	public PatientsView()
 	{
 		InitializeComponent();
+		BindingContext = new PatientViewModel();
 	}
 
 	private void AddPatientClicked(object sender, EventArgs e)
 	{
-
+		Shell.Current.GoToAsync("//AddPatientsView");
 	}
 	private void CancelClicked(object sender, EventArgs e)
 	{
 		Shell.Current.GoToAsync("//MainPage");
 	}
+
+	private void PatientsView_NavigatedTo(object sender, NavigatedToEventArgs e)
+	{
+		(BindingContext as PatientViewModel)?.Refresh();
+    }
 }
