c573cfaf6aed57aae08ce6c29ae978315477a6e1
Aiden Bello <acanine4100@gmail.com>
2025-12-11 23:57:45 -0500

    Created AppointmentServiceProxy to handle list of appointments as a singleton that is thread safe

--- src/Library.MediPortal/Services/AppointmentServiceProxy.cs
+++ src/Library.MediPortal/Services/AppointmentServiceProxy.cs
@@ -1,8 +1,40 @@
 using System;
+using System.ComponentModel;
+using Library.MediPortal.Models;
 
 namespace Library.MediPortal.Services;
 
 public class AppointmentServiceProxy
 {
+    private List<Appointment?> appointmentList;
 
+    private AppointmentServiceProxy()
+    {
+        appointmentList = new List<Appointment?>();
+    }
+
+    private static AppointmentServiceProxy? instance;
+    private static object instanceLock = new object();
+
+    public static AppointmentServiceProxy Current
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                {
+                    instance = new AppointmentServiceProxy();
+                }
+            }
+            return instance;
+        }
+    }
+    public List<Appointment?> Appointments
+    {
+        get
+        {
+            return appointmentList;
+        }
+    }
 }
