a9c84c2a8dc32f22d29aebcfd8e591c9bc125d74
Aiden <105905456+ai-bello@users.noreply.github.com>
2026-05-09 12:51:21 -0400

    Added interface INotifyPropertyChanged
    Added interface INotifyPropertyChanged and added PropertyChanged event to redraw Appointments when the list is modified.

--- src/Maui.MediPortal/ViewModels/AppointmentViewModel.cs
+++ src/Maui.MediPortal/ViewModels/AppointmentViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ using Library.MediPortal.Services;
 
 namespace Maui.MediPortal.ViewModels
 {
-    public class AppointmentViewModel
+    public class AppointmentViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Appointment?> Appointments
         {
@@ -18,5 +19,7 @@ namespace Maui.MediPortal.ViewModels
                 return new ObservableCollection<Appointment?>(AppointmentServiceProxy.Current.Appointments);
             }
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
