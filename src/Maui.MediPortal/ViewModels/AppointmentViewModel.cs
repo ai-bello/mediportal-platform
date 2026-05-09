bdf776ae1eb8a1ccba19bc6df42ccc785bc4768d
Aiden <105905456+ai-bello@users.noreply.github.com>
2026-05-09 12:39:42 -0400

    Created button for adding appointments and modified AppointmentViewModel
    AppointmentViewModel now uses an ObservableCollection for the appointment list because it is dynamic and will notify me when the list is modified.

---
+++ src/Maui.MediPortal/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.MediPortal.Models;
+using Library.MediPortal.Services;
+
+namespace Maui.MediPortal.ViewModels
+{
+    public class AppointmentViewModel
+    {
+        public ObservableCollection<Appointment?> Appointments
+        {
+           get
+            {
+                return new ObservableCollection<Appointment?>(AppointmentServiceProxy.Current.Appointments);
+            }
+        }
+    }
+}
--- src/Maui.MediPortal/Views/AppointmentsView.xaml.cs
+++ src/Maui.MediPortal/Views/AppointmentsView.xaml.cs
@@ -1,3 +1,5 @@
+using Maui.MediPortal.ViewModels;
+
 namespace Maui.MediPortal.Views;
 
 public partial class AppointmentsView : ContentPage
@@ -5,10 +7,16 @@ public partial class AppointmentsView : ContentPage
 	public AppointmentsView()
 	{
 		InitializeComponent();
+		BindingContext = new AppointmentViewModel();
 	}
 
 	private void CancelClicked(object sender, EventArgs e)
 	{
 		Shell.Current.GoToAsync("//MainPage");
+	}
+
+	private void AddClicked(object sender, EventArgs e)
+	{
+
 	}
 }
